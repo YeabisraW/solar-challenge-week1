e9bf4b4e5fecdb7322095a0dd0a74914885da86b
Yeabisra Wubishet <wubyab@gmail.com>
2025-11-12 15:37:06 +0300

    Add placeholder notebook and dummy plot script with generated PNG

---
+++ reports/dummy_plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+# Dummy plot
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.title('Dummy Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.savefig('reports/dummy_plot.png')
+plt.show()
